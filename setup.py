46238a420364de8b92df6f0935b684bb164dec6d
Martin Abente Lahaye <tch@sugarlabs.org>
2019-11-14 16:56:06 -0300

    Release 1.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from setuptools import setup
 
 setup(name='sugarapp',
-      version='1.0',
+      version='1.1',
       description='Port Sugar activities to other desktops',
       url='https://github.com/tchx84/sugarapp',
       author='Martín Abente Lahaye',
